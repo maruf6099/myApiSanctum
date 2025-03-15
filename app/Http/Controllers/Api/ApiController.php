3e1566700af1b230fafc8521e1cb740e0ff0486b
maruf <marufbdn609@gmail.com>
2025-03-15 20:05:18 +0600

    register_function

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function register(Request $request){
+        $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|confirmed",
+        ]);
+
+        User::create($request->all());
+
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
+    }
+    // public function login(){
+
+    // }
+    // public function profile(){
+
+    // }
+    // public function logout(){
+
+    // }
+    
 }
